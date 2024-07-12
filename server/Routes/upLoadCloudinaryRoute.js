f7f0f27f09239f5e180883ad4cffbf1da7c6deaa
ZzinzZ <nhata338@gmail.com>
2024-07-12 17:10:28 +0700

    second commit

--- server/Routes/upLoadCloudinaryRoute.js
+++ server/Routes/upLoadCloudinaryRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/upload',upload.fields([{name: "image", maxCount: 1}]),(req, res) => {
     try{
         console.log(req.files);
-        if(!req.file) return res.status(400).json({message: "No file selected"});
+        if(!req.file) return res.status(400).json({message: "No file selected!"});
         const link = req.files['image'][0];
         res.status(200).json({message: "Image uploaded successfully", data: link});
     } catch(err){
